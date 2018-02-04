d580b0a1dcd9c464f3fb42d5c50c260a8530233d
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2018-02-04 02:38:13 -0500

    Create stack.swift

---
+++ stack.swift
@@ -0,0 +1,17 @@
+struct Stack<Element> {
+    var items = [Element]()
+    mutating func push(_ item: Element) {
+        items.append(item)
+    }
+    mutating func pop() -> Element {
+        return items.removeLast()
+    }
+}
+
+var stackOfStrings = Stack<String>()
+stackOfStrings.push("uno")
+stackOfStrings.push("dos")
+stackOfStrings.push("tres")
+stackOfStrings.push("cuatro")
+print(stackOfStrings.pop())
+print(stackOfStrings.pop())
