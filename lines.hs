78734a659628f6ca1679895a6ef4bf9b54e6b552
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2018-02-10 22:44:25 -0500

    Create lines.hs

---
+++ lines.hs
@@ -0,0 +1,13 @@
+main :: IO ()
+main = do
+    N_temp <- getLine
+    let N = read N_temp :: Int
+
+getMultipleLines :: Int -> IO [String]
+getMultipleLines n
+    | n <= 0 = return []
+    | otherwise = do          
+        x <- getLine         
+        xs <- getMultipleLines (n-1)    
+        let ret = (x:xs)    
+        return ret          
